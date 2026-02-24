ff6e079ec177157a6eb826a232efdda2c977c403
Aaditya Pawar <aadityapawar2006@gmail.com>
2026-02-25 01:14:07 +0530

    Update CORS: allow Vercel frontend origin and explicit methods

--- server/server.js
+++ server/server.js
@@ -18,8 +18,12 @@ connectDB();
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:3000'],
-  credentials: true
+  origin: [
+    'http://localhost:5173',
+    'https://quiz-system-khaki.vercel.app',
+  ],
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true,
 }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
